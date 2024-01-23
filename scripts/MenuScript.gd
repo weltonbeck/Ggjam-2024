f1bd3ac2dae9394cad0ea5e78d7f7c59aff1720e
welton <tomwb1@gmail.com>
2024-01-23 19:43:53 -0300

    menu and pause game

--- scripts/MenuScript.gd
+++ scripts/MenuScript.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
 
-func _on_button_button_down():
+func _on_start_game():
 	GameControler.changeScenne("res://scennes/levels/TesteScenne.tscn")
+
+func _on_quit_game():
+	get_tree().quit()
---
+++ scripts/PauseMenuScript.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+var can_pause = true
+
+func _ready():
+	$CanvasLayer.visible = false
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_cancel") && get_tree().paused == false && can_pause:
+		can_pause = false
+		$CanvasLayer.visible = true
+		$CanvasLayer/ResumeButton.grab_focus()
+		get_tree().paused = true
+		
+
+func _on_unpause_game():
+	get_tree().paused = false
+	$CanvasLayer.visible = false
+	await get_tree().create_timer(0.2).timeout
+	can_pause = true
+
+func _on_quit_game():
+	get_tree().quit()
